8a03b939191ee4ca5fad98703cb83c9c2cf536f7
hungwii <hungwii@163.com>
2020-10-08 10:51:25 +0800

    change the component to none state component

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,51 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {CSSTransition} from 'react-transition-group'
 import {HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button, SearchWrapper} from './style'
 import {connect} from 'react-redux'
 
-class Header extends Component {    
-    render() {
-        return(
-            <HeaderWrapper >
-                <Logo />
-                <Nav>
-                    <NavItem className='active left'>首页</ NavItem>
-                    <NavItem className='left'>下载</ NavItem>
-                    <NavItem className='right'>
-                        <span className='iconfont'>&#xe636;</span>
-                    </ NavItem>
-                    <NavItem className='right'>登陆</ NavItem>
-                    <SearchWrapper>
-                        <CSSTransition 
-                            in={this.props.isFocus}
-                            timeout={200}
-                            classNames='slide'
-                        >
-                            <NavSearch 
-                                className={this.props.isFocus? 'yesFocus': ''}
-                                onFocus={this.props.handleOnFocus} 
-                                onBlur={this.props.handleOnBlur}
-                            
-                            />
-                        </CSSTransition>
-                            <span 
-                                className={this.props.isFocus? 'yesFocus iconfont': 'iconfont'}
-                                >&#xe637;
-                            </span>
+const Header = (props) => {
+    return(
+        <HeaderWrapper >
+            <Logo />
+            <Nav>
+                <NavItem className='active left'>首页</ NavItem>
+                <NavItem className='left'>下载</ NavItem>
+                <NavItem className='right'>
+                    <span className='iconfont'>&#xe636;</span>
+                </ NavItem>
+                <NavItem className='right'>登陆</ NavItem>
+                <SearchWrapper>
+                    <CSSTransition 
+                        in={props.isFocus}
+                        timeout={200}
+                        classNames='slide'
+                    >
+                        <NavSearch 
+                            className={props.isFocus? 'yesFocus': ''}
+                            onFocus={props.handleOnFocus} 
+                            onBlur={props.handleOnBlur}
                         
-                    </SearchWrapper>
-                </Nav>
-                <Addition>
-                    <Button className='reg'>注册</Button>
-                    <Button className='writting'>
-                        <span className='iconfont'>&#xe6e5;</span>
-                        写文章
-                    </Button>
-                </Addition>
-            </HeaderWrapper>
-        )
-    }
+                        />
+                    </CSSTransition>
+                        <span 
+                            className={props.isFocus? 'yesFocus iconfont': 'iconfont'}
+                            >&#xe637;
+                        </span>
+                    
+                </SearchWrapper>
+            </Nav>
+            <Addition>
+                <Button className='reg'>注册</Button>
+                <Button className='writting'>
+                    <span className='iconfont'>&#xe6e5;</span>
+                    写文章
+                </Button>
+            </Addition>
+        </HeaderWrapper>
+    )
 }
+
 
 const mapStateToProps = (state) => {
     return {
