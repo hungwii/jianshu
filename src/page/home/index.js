fee4721ea23c8e26ff41fdc3e14e5c20c92165ab
hungwii <hungwii@163.com>
2020-10-15 10:41:17 +0800

    code refined finished

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import {HomeWrapper, HomeLeft, HomeRight} from './style'
+import {actionCreators} from './store'
+
 
 import Recommend from './Recommend'
 import List from './List'
 import Author from './Author'
+import Guide from './Guide'
 
 class Home extends Component {
     render() {
@@ -13,16 +17,30 @@ class Home extends Component {
                     <img className='banner-img' 
                          src='https://upload.jianshu.io/admin_banners/web_images/5006/537c82a858e7b0739b26ccb64d318f5e2ba6fbab.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
                          alt='banner-img'
-                    ></img>
+                    />
                     <Recommend></Recommend>
                     <List></List>
                 </HomeLeft>
                 <HomeRight>
                     <Author></Author>
+                    <Guide></Guide>
                 </HomeRight>
             </HomeWrapper>
         )
     }
+
+    componentDidMount() {
+        this.props.changeHomeData()
+    }
 }
 
-export default Home
+
+const mapDispatchToProps = (dispatch) => ({
+    changeHomeData() {
+        const action = actionCreators.getHomeInfo()
+        dispatch(action)
+    }
+})
+
+
+export default connect(null, mapDispatchToProps)(Home)
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,7 +2,6 @@ import {combineReducers} from 'redux-immutable'
 import {reducer as headerReducer} from '../common/header/store'
 import {reducer as homeReducer} from '../page/home/store'
 
-console.log(homeReducer)
 const reducer = combineReducers({
     header: headerReducer,
     home: homeReducer,
